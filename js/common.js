3eb0279299a4a7512e822c69b5f31a98d3d8e8ee
trsheff <tr_sheff@ukr.net>
2016-09-06 13:44:41 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(function() {
+    // var close = document.querySelector(".main-nav__toggle");
+    // var toggleMenu = document.querySelector(".toggle__menu");
+    //
+    // close.addEventListener("click" , function (event) {
+    //     event.preventDefault();
+    //     $('toggleMenu').transform({x:'150px'});
+    // })
+});
